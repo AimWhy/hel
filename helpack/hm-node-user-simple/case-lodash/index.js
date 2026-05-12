101ce5c86dd707c063868dd2267a7a2af876fe5a
fancyzhong <fancyzhong@tencent.com>
2026-05-12 18:29:45 +0800

    feat: add update link

--- helpack/hm-node-user-simple/case-lodash/index.js
+++ helpack/hm-node-user-simple/case-lodash/index.js
@@ -44,8 +44,8 @@ async function main3() {
 }
 
 // main 是对原模块做部分重新，实现里仅覆盖 lodash 的 uniq 方法
-main().catch(console.error);
+// main().catch(console.error);
 
 // main2 和 main3 时对模块做整体替换，会导致 lodash 的 omit 方法变成 undefined，因为 my-lodash.js 中没有 omit 方法
-// main2().catch(console.error);
+main2().catch(console.error);
 // main3().catch(console.error);
